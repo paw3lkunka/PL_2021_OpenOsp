b35d92f85dc51613b5167d775cfd4d0a9027fee0
Paul B. Kunka <kunkap@scorp-arch>
2021-09-02 20:41:22 +0200

    Modified clean.sh to wipe out old css files too

--- .sh/clean.sh
+++ .sh/clean.sh
@@ -2,4 +2,5 @@
 dotnet clean
 rm -rf ./*/bin/ # .NET binaries
 rm -rf {.paket,packages,paket-files,paket.lock} # Paket
-rm -rf Client/NodeJs/{node_modules,package-lock.json} # Node
+rm -rf Client/{node_modules,package-lock.json} # Node
+rm -rf Client/wwwroot/css/{normalize.min.css,tailwind.min.css} # CSS
