450837de2e720bb561f532850caafb6d7dead9ac
Paweł Kunka <pawelkunka@pm.me>
2022-02-21 17:31:35 +0100

    Changed appname in package.json, removed unused lines in tailwind config

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -7,16 +7,6 @@ module.exports = {
   darkMode: false,
   theme: {
     extend: {
-      // colors: {
-      //   'openosp': {
-      //     'primary': '#e25c28',
-      //     'secondary': '#adadad',
-      //     'bg': {
-      //       'light': '#2e3838',
-      //       'dark': '#160f29'
-      //     }
-      //   }
-      // },
       backgroundImage: theme => ({
         'firefighters': "url('https://live.staticflickr.com/5463/7415300266_98586c85a4_k.jpg')"
       })
